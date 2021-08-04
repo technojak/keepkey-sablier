46c53ff5279a1cbce108fdf05946334b6f348bed
technojak <technojakd@gmail.com>
2021-08-03 21:07:03 -0600

    fix timer

--- src/pages/Stream/Stream.tsx
+++ src/pages/Stream/Stream.tsx
@@ -146,7 +146,7 @@ export function StreamPage() {
 
     useInterval(() => {
         setStream(formatStreamData(data?.stream));
-    }, 30000);
+    }, 1000);
 
     if (loading || !stream) {
         return (
